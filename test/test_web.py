344cb40fcd26812c99889ecc156261fbe68a7e72
viktoriyabuter <viktoriya.buter@gmail.com>
2025-11-12 17:14:13 +0300

    added tests fail ignore

--- test/test_web.py
+++ test/test_web.py
@@ -1,16 +1,36 @@
 import allure
+import pytest
 from selenium.webdriver.common.by import By
 
 
+
+# ------------------ TEST PASSED ------------------
 @allure.title("Page should have a word in the title")
 def test_main_page_title_should_have_word_in_title(selenium):
     with allure.step("Open the main page"):
         selenium.get("https://en.wikipedia.org/wiki/Software_testing")
 
     with allure.step("Look for a phrase in the title"):
+        assert "Software testing" in selenium.title
+
+
+# ------------------ TEST FAILED ------------------
+@allure.title("Failing test example")
+def test_main_page_title_should_fail(selenium):
+    with allure.step("Open the main page"):
+        selenium.get("https://en.wikipedia.org/wiki/Software_testing")
+    with allure.step("Check wrong phrase in title"):
         assert "Bad search" in selenium.title
 
 
+# ------------------ TEST SKIPPED ------------------
+@allure.title("Skipped test example")
+@pytest.mark.skip(reason="Skipping this test for demonstration")
+def test_skipped_example(selenium):
+    with allure.step("This step won't be executed"):
+        selenium.get("https://en.wikipedia.org/wiki/Software_testing")
+
+# ------------------ TEST BROKEN ------------------
 @allure.title("Page should have a text entry element")
 def test_main_page_should_have_text_entry(selenium):
     with allure.step("Open the main page"):
